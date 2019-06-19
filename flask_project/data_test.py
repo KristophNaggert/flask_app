2382ee2b742b91eb25713dcef87f0c5b7c7f821a
KristophNaggert <knaggert@oberlin.edu>
2019-06-19 15:18:15 -0400

    end of day push

--- flask_project/data_test.py
+++ flask_project/data_test.py
@@ -19,6 +19,18 @@ if (any(df['Sys_Name'] == gene)):
     print(gene)
 else:
     index = next(iter(df[df.Std_Name==gene].index), 'No match.')
-    print (index)
+    print(index)
     gene = df.Sys_Name[index]
     print(gene)
+
+
+'''
+Currently has no way to catch and inform the user of the application
+if the standard gene name or systematic gene name do not exist in the
+local database.
+
+Also need to create a catch if there is no input, becuase there are certain
+entries in the gene list (results.tsv) whose Std_Name entry is ""and the
+application might break if the program tries to find the gene name "" when a
+user sequence is input.
+'''
